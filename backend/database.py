ebd5f47ef0a531fdd8da65e68aac6bad4d223b78
Maneesha <yerraboinamanisha@gmail.com>
2026-08-14 12:34:14 +0530

    Configure backend for Render

--- backend/database.py
+++ backend/database.py
@@ -1,15 +1,12 @@
+import os
 from pymongo import MongoClient
 
-# MongoDB Atlas connection string
-MONGO_URI = "mongodb+srv://taskflow_user:Taskflow%402026@cluster0.klrf1r5.mongodb.net/?appName=Cluster0"
+MONGO_URI = os.getenv("MONGO_URI")
 
-# Create MongoDB client
 client = MongoClient(MONGO_URI)
 
-# Select database
 db = client["taskflow"]
 
-# Collections
 users_collection = db["users"]
 tasks_collection = db["tasks"]
 projects_collection = db["projects"]
